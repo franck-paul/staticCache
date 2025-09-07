91390b31fe57a2bad4b82f39a87f46ac745e57ca
franck <carnet.franck.paul@gmail.com>
2025-09-07 15:45:55 +0200

    DC 2.36 compliance

--- _define.php
+++ _define.php
@@ -19,11 +19,12 @@ $this->registerModule(
     '5.1',
     [
         'date'     => '2003-08-13T13:42:00+0100',
-        'requires' => [['core', '2.33']],
+        'requires' => [['core', '2.36']],
         'type'     => 'plugin',
 
         'details'    => 'https://open-time.net/?q=staticCache',
         'support'    => 'https://github.com/franck-paul/staticCache',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/staticCache/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
